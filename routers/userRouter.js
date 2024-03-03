f9454adb80efab2fd85285cc781ec9ad207f0431
Nhân_Cùi_GòZấp <phamducnhan.it@gmail.com>
2024-03-03 22:54:51 +0700

    feat: create api login

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const Router = express.Router();
-const { getAllUser, createUser, updateUser, deleteUser,register } = require('../controllers/userController');
+const { getAllUser, createUser, updateUser, deleteUser,register,login } = require('../controllers/userController');
 
 Router.get('/users', getAllUser);
 Router.post('/user', createUser);
 Router.put('/user/:id', updateUser);
 Router.delete('/user/:id', deleteUser);
 Router.post('/register', register);
+Router.post('/login', login);
 
 module.exports = Router;
