d87ccff14fcea9e42e301965c10f133f4afa0588
Nhân_Cùi_GòZấp <phamducnhan.it@gmail.com>
2024-03-15 12:22:38 +0700

    feat: create api forgotpassword

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const Router = express.Router();
-const { verifycation} = require('../controllers/authController');
+const { verifycation,forgotPassword} = require('../controllers/authController');
 
 Router.post('/verifycation', verifycation);
+Router.post('/forgotpassword', forgotPassword);
 
 module.exports = Router;
