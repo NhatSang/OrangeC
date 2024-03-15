0eab68532d4165b3f58c07261ef8ac2ddd2cbe42
Nhân_Cùi_GòZấp <phamducnhan.it@gmail.com>
2024-03-15 21:25:26 +0700

    fix: fix controller,router

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Router = express.Router();
 const { verifycation,forgotPassword} = require('../controllers/authController');
 
-Router.post('/verifycation', verifycation);
+Router.post('/auth/verifycation', verifycation);
 Router.post('/forgotpassword', forgotPassword);
 
 module.exports = Router;
