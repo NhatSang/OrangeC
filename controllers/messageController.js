32bb87036328846064dbafd265159b4d7c7b3117
Nhân_Cùi_GòZấp <phamducnhan.it@gmail.com>
2024-04-02 16:38:28 +0700

    fix socket v5

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -16,28 +16,37 @@ const getAllMessage = asyncHandler(async (req, res) => {
   return res.status(200).json({ success: true, data: messages });
 });
 
-const createMessage = asyncHandler(async (msg) => {
-  const message = new Message({
-    conversationId: msg.conversationId,
-    senderId: msg.senderId,
-    receiverId: msg.receiverId,
-    type: msg.type,
-    contentMessage: msg.contentMessage,
-    urlType: msg.urlType,
-    createAt: msg.createAt,
-    isDeleted: msg.isDeleted,
-    reaction: msg.reaction,
-    isSeen: msg.isSeen,
-    isReceive: msg.isReceive,
-    isSend: msg.isSend,
-  });
-  await message.save();
-  await Conversation.updateOne(
-    { _id: msg.conversationId },
-    { $push: { messages: message._id } }
-  );
-  return message;
-});
+const createMessage = async (msg) => {
+  try {
+      const message = new Message({
+          conversationId: msg.conversationId,
+          senderId: msg.senderId,
+          receiverId: msg.receiverId,
+          type: msg.type,
+          contentMessage: msg.contentMessage,
+          urlType: msg.urlType,
+          createAt: msg.createAt,
+          isDeleted: msg.isDeleted,
+          reaction: msg.reaction,
+          isSeen: msg.isSeen,
+          isReceive: msg.isReceive,
+          isSend: msg.isSend,
+      });
+
+      await message.save();
+
+      await Conversation.updateOne(
+          { _id: msg.conversationId },
+          { $push: { messages: message._id } }
+      );
+
+      return message;
+  } catch (error) {
+      console.error("Error creating message:", error);
+      console.log(error);
+      throw error;
+  }
+};
 
 //lấy 20 tin nhắn gần nhất
 const getLastMessage = asyncHandler(async (req, res) => {
